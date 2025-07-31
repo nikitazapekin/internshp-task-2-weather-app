047ad573ca865d55328a7b3c5db2ae708373d625
nikitazapekin <zapekinnikitamain@gmail.con>
2025-07-31 11:43:27 +0300

    feat(aliases): Add alias imports

--- webpack.config.ts
+++ webpack.config.ts
@@ -41,6 +41,15 @@ const config: WebpackConfiguration = {
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@constants': path.resolve(__dirname, 'src/constants'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@store': path.resolve(__dirname, 'src/store'),
+      '@api': path.resolve(__dirname, 'src/api'),
+    },
   },
 };
 
