731b4467b021c2c41a380ea1be4506da8470648a
nikitazapekin <zapekinnikitamain@gmail.con>
2025-07-31 13:59:29 +0300

    feat(ts-loader): Add ts-loader

--- webpack.config.ts
+++ webpack.config.ts
@@ -16,11 +16,15 @@ const config: WebpackConfiguration = {
       {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
-        use: 'babel-loader',
+        use: [
+          'babel-loader',
+          'ts-loader'
+        ]
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ['style-loader',
+          'css-loader'],
       },
     ],
   },
