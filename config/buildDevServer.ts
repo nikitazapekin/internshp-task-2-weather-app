4066d7c402dbaaabe06d1db2f81964d6921c7d8b
nikitazapekin <zapekinnikitamain@gmail.con>
2025-08-01 15:51:06 +0300

    chore(format): Format buildDevServerFile

--- config/buildDevServer.ts
+++ config/buildDevServer.ts
@@ -1,5 +1,5 @@
 import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
-import {BuildOptions} from "./types/types";
+import { BuildOptions } from "./types/types";
 
 export function buildDevServer(options: BuildOptions): DevServerConfiguration {
 
