6ff1aa7efe582bd7897d2e424c0f1fdd288d2185
nikitazapekin <zapekinnikitamain@gmail.con>
2025-08-03 22:10:23 +0300

    fix(jest): Fix jest issue

--- src/helpers/sayHelloFunction.test.ts
+++ src/helpers/sayHelloFunction.test.ts
@@ -1,4 +1,5 @@
 import { sayHello } from "./sayHelloFunction";
+import { expect } from "@jest/globals";
 
 describe("sayHello function", () => {
   it("should return correct greeting for a given name", () => {
