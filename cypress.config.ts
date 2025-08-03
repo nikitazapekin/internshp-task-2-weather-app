0ba70d5f99f764c8123e1b51eaab2ff7f5322c24
nikitazapekin <zapekinnikitamain@gmail.con>
2025-08-03 22:45:11 +0300

    test(App): Add first e2e test

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  video: true,
+  port: 4001,
+  chromeWebSecurity: false,
   e2e: {
+    baseUrl: "http://localhost:4000",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
