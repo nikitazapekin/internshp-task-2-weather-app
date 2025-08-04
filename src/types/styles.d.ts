73cf348a1adc245480d8ae0b3449330ef32ad09b
nikitazapekin <zapekinnikitamain@gmail.com>
2025-08-04 15:51:34 +0300

    feat(styles): Add style.d.file

---
+++ src/types/styles.d.ts
@@ -0,0 +1,37 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    container: number;
+    largeContainer: number;
+    deviceHeight: string;
+    colors: {
+      black: string;
+      blue: string;
+      gray: string;
+      white: string;
+    };
+    fontFamilies: {
+      primary: string;
+      secondary: string;
+    };
+    fontSizes: {
+      xl: string;
+      lg: string;
+      md: string;
+      sm: string;
+      xs: string;
+      xxs: string;
+    };
+    fontWeights: {
+      regular: number;
+    };
+    media: {
+      xxl: string;
+      xl: string;
+      lg: string;
+      md: string;
+      sm: string;
+    };
+  }
+}
