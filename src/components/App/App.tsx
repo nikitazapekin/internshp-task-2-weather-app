c11dbc2c6f13d40a2f170436dd8f3dbe8730346b
nikitazapekin <zapekinnikitamain@gmail.com>
2025-08-04 20:37:06 +0300

    chore(cleanup): Cleanup App.tsx

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,3 @@
-import { fetchWeatherRequest } from "@store/actions/weatherActions";
-import { useDispatch } from "react-redux";
-
 export const App = () => {
-  const dispatch = useDispatch();
-
-  const handleElasticSearch = () => {
-    dispatch(fetchWeatherRequest());
-  };
-
-  return (
-    <div>
-      <button onClick={handleElasticSearch}>test</button>
-    </div>
-  );
+  return <div></div>;
 };
