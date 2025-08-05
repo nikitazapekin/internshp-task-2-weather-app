a285717a82b18154451ea08711300ea66868dcc9
nikitazapekin <zapekinnikitamain@gmail.com>
2025-08-05 23:41:04 +0300

    fix(getCurrentTime): Add validations

--- src/components/Time/index.tsx
+++ src/components/Time/index.tsx
@@ -3,7 +3,11 @@ import { getCurrentTime } from "@utils/helpers/getCurrentTime/getCurrentTime";
 import { Time } from "./styled";
 
 const DateInfo = () => {
-  const { timeString, dateString } = getCurrentTime();
+  const { timeString, dateString, error } = getCurrentTime();
+
+  if (error) {
+    <Time>{error}</Time>;
+  }
 
   return (
     <>
