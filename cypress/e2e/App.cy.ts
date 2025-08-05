cd82d395d56d4b1b6c43636fa1e72c0e232a6773
nikitazapekin <zapekinnikitamain@gmail.com>
2025-08-05 18:07:42 +0300

    chore(clenup): Remove code smell

--- cypress/e2e/App.cy.ts
+++ cypress/e2e/App.cy.ts
@@ -1,14 +1,8 @@
 describe("App Component", () => {
-  beforeEach(() => {
-    cy.visit("/");
-  });
-
   it("should render the App component", () => {
     cy.get("div").should("exist");
   });
-  it('should display "Hello world!" text', () => {
-    cy.contains("Hello world!").should("be.visible");
-  });
+
   it("should have exactly one div element", () => {
     cy.get("div").should("have.length", 1);
   });
