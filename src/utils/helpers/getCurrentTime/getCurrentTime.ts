ea33bcd45f1b94d29087dc9aa9f717716bd027d3
nikitazapekin <zapekinnikitamain@gmail.com>
2025-08-07 00:43:21 +0300

    chore(cicd): Update ci cd

--- src/utils/helpers/getCurrentTime/getCurrentTime.ts
+++ src/utils/helpers/getCurrentTime/getCurrentTime.ts
@@ -1,4 +1,4 @@
-import { days, FIRST_DAY_OF_MONTH, LAST_DAY_OF_MONTH, months } from "@constants";
+import { days, FIRST_DAY_OF_MONTH, LAST_DAY_OF_MONTH, months } from "@constants/monthsAndDays";
 
 interface TimeAndDate {
   timeString: string;
