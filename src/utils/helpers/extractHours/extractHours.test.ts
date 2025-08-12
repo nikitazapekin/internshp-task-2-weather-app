8a602b284091f482b2b4409c860deceac91b7aaa
nikitazapekin <zapekinnikitamain@gmail.com>
2025-08-12 14:11:41 +0300

    refactor(test): Refactor test assertion

--- src/utils/helpers/extractHours/extractHours.test.ts
+++ src/utils/helpers/extractHours/extractHours.test.ts
@@ -3,17 +3,6 @@ import { expect } from "@jest/globals";
 import { extractTime } from "./extractHours";
 
 describe("extractTime function", () => {
-  it("should return time in HH:MM format", () => {
-    const testDate1 = "2025-05-15T14:30:00Z";
-    const result1 = extractTime(testDate1);
-
-    expect(result1).toBe("17:30");
-
-    const testDate3 = "2025-05-15T00:00:00Z";
-    const result3 = extractTime(testDate3);
-
-    expect(result3).toBe("3:00");
-  });
   it("should return string type", () => {
     const testDate = "2025-05-15T12:34:00Z";
     const result = extractTime(testDate);
