bd67232c8303677cdb80ae080ef300afd27c36e2
nikitazapekin <zapekinnikitamain@gmail.com>
2025-08-14 23:07:10 +0300

    fix(reducer): Fix reducer

--- src/store/sagas/currentCity.ts
+++ src/store/sagas/currentCity.ts
@@ -1,5 +1,4 @@
 import type { OpenWeatherGeoResponse } from "@types/CitySearchResponseTypes";
-import type { CurrentCoordinatsState } from "@types/coordinatsTypes";
 import { getErrorMessage } from "@utils/helpers/getErrorMessage/getErrorMessage";
 import type { AxiosResponse } from "axios";
 import type { SagaIterator } from "redux-saga";
@@ -15,15 +14,10 @@ import {
 function* fetchCurrentCity(action: ReturnType<typeof fetchCurrentCityRequest>): SagaIterator {
   try {
     const { latitude, longitude } = action.payload;
-    const params: CurrentCoordinatsState = {
-      latitude,
-      longitude,
-      isGeolocationDenied: false,
-    };
 
     const response: AxiosResponse<OpenWeatherGeoResponse> = yield call(
-      [WeatherService, "getCityNameByCoordinats"],
-      params
+      WeatherService.getCityNameByCoordinats.bind(WeatherService),
+      { latitude, longitude }
     );
 
     if (Array.isArray(response.data) && response.data.length > 0) {
@@ -41,3 +35,43 @@ function* fetchCurrentCity(action: ReturnType<typeof fetchCurrentCityRequest>): 
 export function* CurrentCity(): SagaIterator {
   yield takeLatest(fetchCurrentCityRequest.type, fetchCurrentCity);
 }
+/* import { ERROR_CONSTANTS } from "@constants/errors";
+import type { OpenWeatherGeoResponse } from "@types/CitySearchResponseTypes";
+import type { CurrentCoordinatsState } from "@types/coordinatsTypes";
+import type { SagaIterator } from "redux-saga";
+import { call, put, takeLatest } from "redux-saga/effects";
+
+import WeatherService from "@api/weatherService";
+import {
+  fetchCurrentCityFailure,
+  fetchCurrentCityRequest,
+  fetchCurrentCitySuccess,
+} from "@store/actions/currentCity";
+
+function* fetchCurrentCity(action: ReturnType<typeof fetchCurrentCityRequest>): SagaIterator {
+  try {
+    const { latitude, longitude } = action.payload;
+
+    const response: { data: OpenWeatherGeoResponse } = yield call(
+      [WeatherService, "getCityNameByCoordinats"],
+      { latitude, longitude } as CurrentCoordinatsState
+    );
+
+    if (Array.isArray(response.data) && response.data.length > 0) {
+      const cityName = response.data[0].name;
+
+      yield put(fetchCurrentCitySuccess(cityName));
+    } else {
+      yield put(fetchCurrentCityFailure(ERROR_CONSTANTS.NO_CITY_FOUND));
+    }
+  } catch (error: unknown) {
+    const message = error instanceof Error ? error.message : ERROR_CONSTANTS.UNKNOWN_ERROR;
+
+    yield put(fetchCurrentCityFailure(message));
+  }
+}
+
+export function* CurrentCity(): SagaIterator {
+  yield takeLatest(fetchCurrentCityRequest.type, fetchCurrentCity);
+}
+ */
