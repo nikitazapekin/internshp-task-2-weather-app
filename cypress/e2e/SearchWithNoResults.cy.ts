de1c2cf6a73e50b06cb3c2591748f4624094f65d
nikitazapekin <zapekinnikitamain@gmail.com>
2025-08-18 21:20:27 +0300

    feat(test): Add tests

--- cypress/e2e/SearchWithNoResults.cy.ts
+++ cypress/e2e/SearchWithNoResults.cy.ts
@@ -16,7 +16,7 @@ describe("SearchCitiesComponent", () => {
     cy.get("input").type("dddddddddddddddddddddddddddddddddd");
     cy.get('[data-testid="spinner"]').should("exist");
 
-    cy.wait("@getCities", { timeout: 10000 }).then((interception) => {
+    cy.wait("@getCities").then((interception) => {
       expect(interception.request.url).to.include("/geo/1.0/direct");
       expect(interception.request.method).to.equal("GET");
 
@@ -28,30 +28,3 @@ describe("SearchCitiesComponent", () => {
     cy.get('[data-testid="no-suggestions"]').should("exist");
   });
 });
-
-/* 
-describe("SearchCitiesComponent", () => {
-  const mockResponse: unknown = [];
-  beforeEach(() => {
-    cy.intercept("GET", `${Cypress.env("REACT_APP_API_URL")}/geo/1.0/direct*`, {
-      statusCode: 200,
-      body: mockResponse,
-    }).as("getCities");
-    cy.visit("http://localhost:4000");
-  });
-
-  it("should validate API response structure matches mock data", () => {
-
-
-    cy.get("input").type("dddddddddddddddddddddddddddddddddd");
-    cy.get('[data-testid="spinner"]').should("exist");
-    cy.wait("@getCities").then((interception) => {
-      const response = interception.response?.body;
-
-      expect(response).to.be.an("array");
-    });
-    cy.get('[data-testid="spinner"]').should("not.exist");
-    cy.get('[data-testid="no-suggestions"]').should("exist");
-  });
-});
- */
--- cypress/e2e/SearchWithSuccessResponse.cy.ts
+++ cypress/e2e/SearchWithSuccessResponse.cy.ts
@@ -1,6 +1,39 @@
 describe("SearchCitiesComponent", () => {
+  const mockResponse = [
+    {
+      name: "London",
+      country: "GB",
+      lat: 51.5074,
+      lon: -0.1278,
+      state: "England",
+      local_names: {
+        en: "London",
+        ru: "Лондон",
+      },
+    },
+  ];
+
   beforeEach(() => {
+    cy.intercept("GET", "**/geo/1.0/direct*", { statusCode: 200, body: mockResponse }).as(
+      "getCities"
+    );
+
     cy.visit("http://localhost:4000");
+  });
+
+  it("should pass correct parameters to API", () => {
+    cy.get("input").type("London");
+
+    cy.wait("@getCities").then((interception) => {
+      expect(interception.request.url).to.include("q=London");
+      expect(interception.request.url).to.include("limit=5");
+    });
+  });
+
+  it("should display suggestions when typing", () => {
+    cy.get("input").type("Lon");
+    cy.get('[data-testid="suggestions-wrapper"]').should("be.visible");
+    cy.get("li").should("have.length.gt", 0);
   });
 
   it("should render search input and button", () => {
@@ -8,59 +41,7 @@ describe("SearchCitiesComponent", () => {
     cy.get("button").should("contain", "Search");
   });
 
-  it("should pass correct parameters to getCitiesByQuery", () => {
-    const mockResponse = [
-      {
-        name: "London",
-        country: "GB",
-        lat: 51.5074,
-        lon: -0.1278,
-        state: "England",
-        local_names: {
-          en: "London",
-          ru: "Лондон",
-        },
-      },
-    ];
-
-    cy.intercept("GET", `${Cypress.env("REACT_APP_API_URL")}/geo/1.0/direct*`, (req) => {
-      expect(req.query.q).to.eq("London");
-      expect(req.query.limit).to.eq("5");
-      expect(req.query.units).to.eq("metric");
-      expect(req.query.lang).to.eq("ru");
-      expect(req.query.appid).to.exist;
-
-      req.reply({
-        statusCode: 200,
-        body: mockResponse,
-      });
-    }).as("apiRequest");
-
-    cy.get("input").type("London");
-
-    cy.wait("@apiRequest");
-  });
-
   it("should validate API response structure matches mock data", () => {
-    const mockResponse = [
-      {
-        name: "London",
-        country: "GB",
-        lat: 51.5074,
-        lon: -0.1278,
-        state: "England",
-        local_names: {
-          en: "London",
-          ru: "Лондон",
-        },
-      },
-    ];
-
-    cy.intercept("GET", `${Cypress.env("REACT_APP_API_URL")}/geo/1.0/direct*`, {
-      statusCode: 200,
-      body: mockResponse,
-    }).as("getCities");
-
     cy.get("input").type("London");
 
     cy.wait("@getCities").then((interception) => {
@@ -80,7 +61,6 @@ describe("SearchCitiesComponent", () => {
         expect(response[0].local_names.en).to.be.a("string");
         expect(response[0].local_names.ru).to.be.a("string");
       }
-
       expect(response[0].lat).to.be.within(-90, 90);
       expect(response[0].lon).to.be.within(-180, 180);
     });
@@ -88,15 +68,15 @@ describe("SearchCitiesComponent", () => {
     cy.get('[data-testid="suggestions-wrapper"]').should("exist");
   });
 
-  it("should display suggestions when typing", () => {
-    cy.get("input").type("Lon");
-    cy.get('[data-testid="suggestions-wrapper"]').should("be.visible");
-    cy.get("li").should("have.length.gt", 0);
-  });
-
   it("should select a suggestion when clicked", () => {
     cy.get("input").type("Lon");
     cy.get("li").first().click();
     cy.get("input").should("have.value", "London, England, GB");
+  });
+
+  it("should display suggestions when typing", () => {
+    cy.get("input").type("Lon");
+    cy.get('[data-testid="suggestions-wrapper"]').should("be.visible");
+    cy.get("li").should("have.length.gt", 0);
   });
 });
