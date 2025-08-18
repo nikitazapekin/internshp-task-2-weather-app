b062c1d91641b249eba257d4b29a82578fa038ca
nikitazapekin <zapekinnikitamain@gmail.com>
2025-08-18 15:27:43 +0300

    feat(test): Add search tests

---
+++ cypress/e2e/DisplayOfElements.cy.ts
@@ -0,0 +1,29 @@
+describe("Weather Page Component", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:4000");
+  });
+
+  it("should have banner element", () => {
+    cy.get('[data-testid="banner"]').should("exist");
+  });
+
+  it("should have search input", () => {
+    cy.get("input").should("have.length", 1);
+  });
+
+  it("should have url /", () => {
+    cy.url().should("include", "/");
+  });
+
+  it("should have Sign in button", () => {
+    cy.contains("button", "Sign in").should("exist");
+  });
+
+  it("should have Daily forecast button", () => {
+    cy.contains("button", "Daily").should("exist");
+  });
+
+  it("should have Hourly forecast button", () => {
+    cy.contains("button", "Hourly").should("exist");
+  });
+});
--- cypress/e2e/Search.cy.ts
+++ cypress/e2e/Search.cy.ts
@@ -8,8 +8,42 @@ describe("SearchCitiesComponent", () => {
     cy.get("button").should("contain", "Search");
   });
 
-  it("should show suggestions when typing in the input", () => {
-    cy.get("input").type("London");
+  it("should show suggestions when typing in the input and make correct API request", () => {
+    const mockResponse = [
+      {
+        name: "London",
+        country: "GB",
+        lat: 51.5074,
+        lon: -0.1278,
+      },
+    ];
+
+    cy.intercept("GET", "https://api.openweathermap.org/geo/1.0/direct*", (req) => {
+      expect(req.url).to.match(/q=te/);
+      expect(req.url).to.match(/limit=5/);
+      expect(req.url).to.match(/units=metric/);
+      expect(req.url).to.match(/lang=ru/);
+      expect(req.url).to.match(/appid=[a-zA-Z0-9]+/);
+
+      expect(req.url).to.match(/q=te&limit=5&appid=.+&units=metric&lang=ru/);
+
+      req.reply({
+        statusCode: 200,
+        body: mockResponse,
+      });
+    }).as("getCities");
+
+    cy.get("input").type("te");
+
+    cy.get('[data-testid="spinner"]').should("exist");
+
+    cy.wait("@getCities").then((interception) => {
+      expect(interception.request.url).to.eq(
+        "https://api.openweathermap.org/geo/1.0/direct?q=te&limit=5&appid=cda8d711f59b4dd43f8ba261a26ec7a9&units=metric&lang=ru"
+      );
+    });
+
+    cy.get('[data-testid="spinner"]').should("not.exist");
     cy.get('[data-testid="suggestions-wrapper"]').should("exist");
   });
 
